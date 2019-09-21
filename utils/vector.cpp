f3585ec5c67d6ef4d26c9a44cd7444b39a103cc0
tenmakamatani <tenma_18n1102540@nnn.ed.jp>
2019-09-21 16:08:59 +0900

    [add]配列の和

--- utils/vector.cpp
+++ utils/vector.cpp
@@ -1,8 +1,6 @@
 // よく使う処理系をまとめる
 #include <stdio.h>
 #include <vector>
-#include <algorithm>
-#include <functional>
 
 
 // 要素数が4のint型の配列を宣言
@@ -18,8 +16,15 @@ nums.size();
 
 
 // 配列を昇順にソート
+#include <algorithm>
 sort(nums.begin(), nums.end());
 
 
 // 配列を降順にソート
+#include <algorithm>
+#include <functional>
 sort(nums.begin(), nums.end(), greater<int>());
+
+// 配列の合計を求める
+#include <numeric>
+accumulate(nums.begin(), nums.end(), 0);
