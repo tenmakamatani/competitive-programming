56a61d739befbb1ccd828105b9de0426deb8a5e6
tenmakamatani <tenma_18n1102540@nnn.ed.jp>
2019-10-02 12:21:47 +0900

    a

---
+++ ABC138/a.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < n; i++)
+using namespace std;
+typedef long long ll;
+
+int main() {
+    int a;
+    string s;
+    cin >> a >> s;
+    if (a >= 3200) {
+        cout << s << endl;
+    } else {
+        cout << "red" << endl;
+    }
+}
